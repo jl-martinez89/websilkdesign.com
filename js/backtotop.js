994e4fb27d97c2fc3e7df059e5ebebb03c8351df
Jose Martinez <jmartinezg.ca@gmail.com>
2018-04-19 10:40:00 -0700

    Version 1.4

---
+++ js/backtotop.js
@@ -0,0 +1,14 @@
+$( document ).ready(function() {
+  $(window).scroll(function() {
+    if ($(this).scrollTop() >= 150) {
+      $('#return-to-top').fadeIn(200);
+    } else {
+      $('#return-to-top').fadeOut(200);
+    }
+  });
+  $('#return-to-top').click(function() {
+    $('body,html').animate({
+      scrollTop : 0
+    }, 1300);
+  });
+});
